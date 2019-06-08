efdeed9ac713fd1ab022bf21fc2342843780d226
Nestor Kauil Xix <neto1xix@gmail.com>
2019-06-08 04:22:41 -0500

    [CLEANUP Limpieza de codigo, se paso el eslint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export default() => {
-  return axios.create({
-    baseURL: 'https://experienciasxcaret.github.io/Developers/api',
-    withCredentials: false,
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
-}
+export default () => axios.create({
+  baseURL: 'https://experienciasxcaret.github.io/Developers/api',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
