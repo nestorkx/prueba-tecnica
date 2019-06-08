bf66dbe764dcf1278120337fb241644955f112dd
Nestor Kauil Xix <neto1xix@gmail.com>
2019-06-08 03:09:09 -0500

    [FEATURE] Agregado nuevos layouts al home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <Header />
+    <MainContent />
+    <Banners />
     <WhyBuy />
     <FooterAbout />
     <ContactPhone />
@@ -14,11 +16,15 @@ import Header from '@/layouts/Header.vue';
 import Footer from '@/layouts/Footer.vue';
 import ContactPhone from '@/layouts/ContactPhone.vue';
 import FooterAbout from '@/layouts/FooterAbout.vue';
-import WhyBuy from '@/layouts/WhyBuy';
+import WhyBuy from '@/layouts/WhyBuy.vue';
+import Banners from '@/layouts/Banners.vue';
+import MainContent from '@/layouts/MainContent.vue';
 
 export default {
   name: 'home',
   components: {
+    MainContent,
+    Banners,
     WhyBuy,
     ContactPhone,
     Footer,
