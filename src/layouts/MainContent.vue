117ef87a9596743c3d86adcfc15d0e8904178e7b
Nestor Kauil Xix <neto1xix@gmail.com>
2019-06-10 04:54:13 -0500

    [FEATURE] Agregado markup y clases al layout MainContent

--- src/layouts/MainContent.vue
+++ src/layouts/MainContent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="content py-4">
+    <div class="py-4">
       <div class="container">
         <div class="row">
             <div class="col-12 text-center">
@@ -14,10 +14,10 @@
 
         <div class="row">
           <div class="col-12">
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-              <li class="nav-item">
+            <ul class="nav nav-tabs custom__nav--tabs" id="myTab" role="tablist">
+              <li class="nav-item custom__nav--item text-center">
                 <a
-                  class="nav-link active"
+                  class="nav-link custom__nav--tabs-link active"
                   id="home-tab"
                   data-toggle="tab"
                   href="#home"
@@ -27,8 +27,9 @@
                   Compra tu admisi&oacute;n
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link"
+              <li class="nav-item custom__nav--item text-center">
+                <a
+                  class="nav-link custom__nav--tabs-link"
                    id="profile-tab"
                    data-toggle="tab"
                    href="#profile"
@@ -38,9 +39,9 @@
                   Atracciones
                 </a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item custom__nav--item text-center">
                 <a
-                  class="nav-link"
+                  class="nav-link custom__nav--tabs-link"
                   id="contact-tab"
                   data-toggle="tab"
                   href="#contact"
@@ -54,7 +55,7 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-8" style="border: 1px solid blue;">
+          <div class="col-xl-8 col-sm-12">
             <div class="tab-content" id="myTabContent">
               <div
                 class="tab-pane fade show active"
@@ -74,8 +75,8 @@
                 aria-labelledby="contact-tab">contact</div>
             </div>
           </div>
-          <div class="col-4" style="border: 1px solid red;">
-            Aqui va el motor de reserva
+          <div class="col-xl-4 col-sm-12">
+            <BookingEngine/>
           </div>
         </div>
       </div>
@@ -84,9 +85,13 @@
 
 <script>
 import Admision from '@/components/Admision.vue';
+import BookingEngine from '@/components/BookingEngine.vue';
 
 export default {
   name: 'MainContent',
-  components: { Admision },
+  components: {
+    BookingEngine,
+    Admision,
+  },
 };
 </script>
